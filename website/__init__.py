c2f0018468f06c3d705398ab9b74273b677ab194
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-11-19 12:56:22 +0000

    Created main page

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,8 @@ from hidden_details import Details
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = Details.flask_secret_key()
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
     return app
